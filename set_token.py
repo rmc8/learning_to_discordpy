0198566ac0bb0dcdedc036d8db668eb8a5a751fd
rmc8 <kmprmc@gmail.com>
2023-03-16 16:54:35 +0900

    fix set env val

--- set_token.py
+++ set_token.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 
 def write_val(val):
-    dotenv_path: str = join(dirname(__file__), '.env')
+    dotenv_path: str = join(dirname(__file__), ".env")
     with open(dotenv_path, "a", encoding="utf-8") as f:
         print(val, file=f)
 
